d5f35b87db19aacd6a295efaea88dd8d05937860
ndevln <github@ndevln.org>
2025-03-11 21:36:56 +0100

    rename theme helpers

--- R/ggplot_theme_helpers.R
+++ R/ggplot_theme_helpers.R
@@ -1,29 +1,29 @@
 #' Quickly remove the minor lines of the panel grid
 #'
 #' @description
-#' `th_remove_panel_grid_minor()`, `th_remove_panel_grid_minor_x()`,
-#' `th_remove_panel_grid_minor_y()` are convenience functions remove the minor lines
+#' `theme_mod_remove_minor_grid()`, `theme_mod_remove_minor_grid_x()`,
+#' `theme_mod_remove_minor_grid_y()` are convenience functions remove the minor lines
 #' of the panel grid.
 #' Has to be called after setting the theme.
 #' @return Changes the `panel.grid.minor` of the [ggplot2::theme()].
 #' @export
 # TODO: https://pkgdown.r-lib.org/reference/build_reference.html
-th_remove_panel_grid_minor <- function() {
+theme_mod_remove_minor_grid <- function() {
   ggplot2::theme(panel.grid.minor = ggplot2::element_blank())
 }
-#' @rdname th_remove_panel_grid_minor
+#' @rdname theme_mod_remove_minor_grid
 #' @export
-th_remove_panel_grid_minor_y <- function() {
+theme_mod_remove_minor_grid_y <- function() {
   ggplot2::theme(panel.grid.minor.y = ggplot2::element_blank())
 }
-#' @rdname th_remove_panel_grid_minor
+#' @rdname theme_mod_remove_minor_grid
 #' @export
-th_remove_panel_grid_minor_x <- function() {
+theme_mod_remove_minor_grid_x <- function() {
   ggplot2::theme(panel.grid.minor.x = ggplot2::element_blank())
 }
-#' @rdname th_remove_panel_grid_minor
+#' @rdname theme_mod_remove_minor_grid
 #' @export
-th_remove_panel_grid <- function() {
+theme_mod_remove_panel_grid <- function() {
   ggplot2::theme(panel.grid = ggplot2::element_blank())
 }
 
@@ -35,33 +35,33 @@ th_remove_panel_grid <- function() {
 #' @param margin_top Used to move the tick labels downwards to prevent text intersecting the x-axis.
 #' Increase for multiline text (e.g. 5 for two lines).
 #' @param hjust,vjust Text justification within the rotated text element. Just ignore.
-#' @param ... Arguments passed to `th_rotate_x_axis_labels` and [ggplot2::element_text()].
+#' @param ... Arguments passed to `theme_mod_rotate_x_axis_labels` and [ggplot2::element_text()].
 #' @return Changes the rotation of the axis labels by modifying the `axis.text` of the [ggplot2::theme()].
 #' @export
-#' @name th_rotate_axis_labels
-#' @rdname th_rotate_axis_labels
-th_rotate_x_axis_labels <- function(angle = 90, margin_top = 2, vjust = 0.4, hjust = 0, ...) {
+#' @name theme_mod_rotate_axis_labels
+#' @rdname theme_mod_rotate_axis_labels
+theme_mod_rotate_x_axis_labels <- function(angle = 90, margin_top = 2, vjust = 0.4, hjust = 0, ...) {
   ggplot2::theme(axis.text.x = ggplot2::element_text(
     angle = -angle, vjust = vjust, hjust = hjust,
     margin = margin(t = margin_top)
   ), ...)
 }
-#' @rdname th_rotate_axis_labels
+#' @rdname theme_mod_rotate_axis_labels
 #' @export
-th_rotate_x_axis_labels_90 <- function(angle = 90, ...) th_rotate_x_axis_labels(angle = angle)
-#' @rdname th_rotate_axis_labels
+theme_mod_rotate_x_axis_labels_90 <- function(angle = 90, ...) theme_mod_rotate_x_axis_labels(angle = angle)
+#' @rdname theme_mod_rotate_axis_labels
 #' @export
-th_rotate_x_axis_labels_45 <- function(angle = 45, ...) th_rotate_x_axis_labels(angle = angle)
-#' @rdname th_rotate_axis_labels
+theme_mod_rotate_x_axis_labels_45 <- function(angle = 45, ...) theme_mod_rotate_x_axis_labels(angle = angle)
+#' @rdname theme_mod_rotate_axis_labels
 #' @export
-th_rotate_x_axis_labels_30 <- function(angle = 30, ...) th_rotate_x_axis_labels(angle = angle)
-#' @rdname th_rotate_axis_labels
+theme_mod_rotate_x_axis_labels_30 <- function(angle = 30, ...) theme_mod_rotate_x_axis_labels(angle = angle)
+#' @rdname theme_mod_rotate_axis_labels
 #' @export
-th_rotate_x_axis_labels_60 <- function(angle = 60, ...) th_rotate_x_axis_labels(angle = angle)
+theme_mod_rotate_x_axis_labels_60 <- function(angle = 60, ...) theme_mod_rotate_x_axis_labels(angle = angle)
 # testthat::expect_identical(theme_rotate_x_axis_labels(), theme(axis.text.x = element_text(angle = -90, vjust = 0.5)))
-#' @rdname th_rotate_axis_labels
+#' @rdname theme_mod_rotate_axis_labels
 #' @export
-th_rotate_y_axis_labels <- function(angle = 90, hjust = 0.5, vjust = 0, ...) {
+theme_mod_rotate_y_axis_labels <- function(angle = 90, hjust = 0.5, vjust = 0, ...) {
   ggplot2::theme(axis.text.y = ggplot2::element_text(angle = angle, hjust = hjust, vjust = vjust, ...))
 }
 
@@ -76,62 +76,61 @@ th_rotate_y_axis_labels <- function(angle = 90, hjust = 0.5, vjust = 0, ...) {
 #' @param position.inside Coordinates for the legend inside the plot.
 #' If set overwrites `position` to `inside`.
 #' @return Changes the `legend.position` of the [ggplot2::theme()].
-#' @name th_disable_legend, th_legend_position
-#' @rdname th_disable_legend
+#' @name theme_mod_disable_legend, theme_mod_legend_position
+#' @rdname theme_mod_disable_legend
 #' @export
-th_disable_legend <- function() ggplot2::theme(legend.position = "none")
-#' @rdname th_disable_legend
+theme_mod_disable_legend <- function() ggplot2::theme(legend.position = "none")
+#' @rdname theme_mod_disable_legend
 #' @export
-th_legend_position <- function(
+theme_mod_legend_position <- function(
     position = c("top", "bottom", "left", "right", "none", "inside"), position.inside = NULL) {
   position <- match.arg(position)
   if (!is.null(position.inside)) position <- "inside"
   # TODO: Check if position.inside is vector
   ggplot2::theme(legend.position = position, legend.position.inside = position.inside)
 }
-#' @rdname th_disable_legend
+#' @rdname theme_mod_disable_legend
 #' @export
-th_legend_top <- function() ggplot2::theme(legend.position = "top")
-#' @rdname th_disable_legend
+theme_mod_legend_top <- function() ggplot2::theme(legend.position = "top")
+#' @rdname theme_mod_disable_legend
 #' @export
-th_legend_bottom <- function() ggplot2::theme(legend.position = "bottom")
-#' @rdname th_disable_legend
+theme_mod_legend_bottom <- function() ggplot2::theme(legend.position = "bottom")
+#' @rdname theme_mod_disable_legend
 #' @export
-th_legend_left <- function() ggplot2::theme(legend.position = "left")
-#' @rdname th_disable_legend
+theme_mod_legend_left <- function() ggplot2::theme(legend.position = "left")
+#' @rdname theme_mod_disable_legend
 #' @export
-th_legend_right <- function() ggplot2::theme(legend.position = "right")
-
+theme_mod_legend_right <- function() ggplot2::theme(legend.position = "right")
 
 #' Quick legend helpers
 #'
 #' @description
-#'  - `th_guide_thick_lines()`: Increase the line thickness for `geom_line()` **colour** legends.
-#'  - `th_guide_long_lines()`: Increase the line thickness for `geom_line()` **linetype** legends.
-#'  - `th_remove_legend_title()`: Removes the legend title. Use after setting the theme.
+#'  - `theme_mod_guide_thick_lines()`: Increase the line thickness for `geom_line()` **colour** legends.
+#'  - `theme_mod_guide_long_lines()`: Increase the line thickness for `geom_line()` **linetype** legends.
+#'  - `theme_mod_remove_legend_title()`: Removes the legend title. Use after setting the theme.
 #' @param linewidth Line width of the line shown in the colour legend.
 #' @param keywidth Key width of the line shown in the legend.
 #' @param ... further parameters passed to [ggplot2::guide_legend()].
 #' @return Overrides the `aes` of the colour guide using [ggplot2::guides()].
 #' @name Quick legend helpers
-#' @rdname th_legend_helpers
+#' @rdname theme_mod_legend_helpers
 NULL
 
-#' @rdname th_legend_helpers
+#' @rdname theme_mod_legend_helpers
 #' @export
-th_guide_thick_lines <- function(linewidth = 3, ...) {
+theme_mod_guide_thick_lines <- function(linewidth = 3, ...) {
   ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(linewidth = linewidth), ...))
 }
 
-#' @rdname th_legend_helpers
+#' @rdname theme_mod_legend_helpers
 #' @export
-th_guide_long_lines <- function(keywidth = 3, ...) {
+theme_mod_guide_long_lines <- function(keywidth = 3, ...) {
   # TODO: nur Lintype oder alle mit theme?
   ggplot2::guides(linetype = ggplot2::guide_legend(keywidth = keywidth, ...))
 }
 
-#' @rdname th_legend_helpers
+#' @rdname theme_mod_legend_helpers
 #' @export
-th_remove_legend_title <- function() {
+theme_mod_remove_legend_title <- function() {
   ggplot2::theme(legend.title = ggplot2::element_blank())
 }

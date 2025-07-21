442104552696570791d9215054ca91b983b4b008
Alexander Bartel <alexander.bartel@lageso.berlin.de>
2025-07-21 12:46:45 +0200

    Polish geom_col_range()

--- R/geom_bar_range.R
+++ R/geom_bar_range.R
@@ -42,12 +42,7 @@ geom_col_range <- function(mapping = NULL, data = NULL,
 }
 
 GeomBarRange <- ggplot2::ggproto("GeomBarRange", GeomBar,
-  required_aes = c("x|y"), # , "xmin|ymin", "xmax|ymax"
-
-  # These aes columns are created by setup_data(). They need to be listed here so
-  # that GeomRect$handle_na() properly removes any bars that fall outside the defined
-  # limits, not just those for which x and y are outside the limits
-  non_missing_aes = c("xmin", "xmax", "ymin", "ymax"),
+  required_aes = c("x|y", "ymin|xmin", "ymax|xmax"), # Either x + ymin/ymax or y + xmin/xmax
   default_aes = aes(!!!GeomRect$default_aes, width = 0.9),
   setup_params = function(data, params) {
     # Stat diverging returns x, xmax, xmin for flipped aes,
